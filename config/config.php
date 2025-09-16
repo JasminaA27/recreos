4a6cb09492d24a48a299f5bf51ccd459876da635
JasminaA27 <176046670+JasminaA27@users.noreply.github.com>
2025-09-16 08:27:38 -0500

    lll

--- config/config.php
+++ config/config.php
@@ -1,9 +1,12 @@
 <?php
 // Configuración de la base de datos
+
+use function PHPSTORM_META\map;
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'recreoss');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', '');
 
 // Configuración de la aplicación
 define('BASE_URL', 'http://localhost:8888/recreos');
@@ -12,4 +15,5 @@ define('SITE_NAME', 'Sistema de Gestión de Recreos');
 // Configuración de sesión
 ini_set('session.cookie_lifetime', 0);
 session_set_cookie_params(0);
+
 ?>
